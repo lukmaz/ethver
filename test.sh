466d1ed053d940813f4e626de5e35105b4574212
Łukasz Mazurek <l.mazurek@mimuw.edu.pl>
2018-07-06 15:42:35 +0200

    Test setups specified in the paper

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-# ./MakeEthver examples/micro_v1.etv
-# ./MakeEthver examples/micro_v2.etv
+./MakeEthver examples/micro_v1.etv
+./MakeEthver examples/micro_v2.etv
+./MakeEthver examples/rps_v1.etv
 ./MakeEthver examples/rps_v2.etv
+./MakeEthver examples/rps_v3.etv
+./MakeEthver examples/rps_v4.etv
